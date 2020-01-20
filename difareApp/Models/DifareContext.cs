32eba56013e4167405610770003ecc7e1f110000
israeldavid76 <israeldavid@gmail.com>
2020-01-20 17:35:27 -0500

    first commit

---
+++ difareApp/Models/DifareContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace difareApp.Models
+{
+    public class DifareContext : DbContext
+    {
+        public DifareContext(DbContextOptions<DifareContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DifareContext> BannerItems { get; set; }
+
+    }
+}
